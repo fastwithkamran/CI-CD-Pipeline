10e23e09c3a0e4e8f1adff644edf078413b44a98
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-09-03 22:02:42 +0500

    Create test.py with eslint errors

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import math
+
+def calculate_area( radius ):
+    Area = 3.14 * radius ** 2
+    return Area
+
+def PrintMessage(msg):
+        print(msg) 
+
+x = 10
+y = 20
+if x == 10:
+ print("X is ten")
