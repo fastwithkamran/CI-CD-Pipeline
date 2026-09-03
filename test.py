33a59d0690fce717edaf11f4d2aaa1e27fb12e30
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-09-03 22:08:02 +0500

    Update test.py with no eslint errors
    Refactor calculate_area and print_message functions for consistency and style.

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
-import os
-import sys
-import math
+def calculate_area(radius):
+    area = 3.14 * radius**2
+    return area
 
-def calculate_area( radius ):
-    Area = 3.14 * radius ** 2
-    return Area
 
-def PrintMessage(msg):
-        print(msg) 
+def print_message(msg):
+    print(msg)
+
 
 x = 10
 y = 20
 if x == 10:
- print("X is ten")
+    print("X is ten")
